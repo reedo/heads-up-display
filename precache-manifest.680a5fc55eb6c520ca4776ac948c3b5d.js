115e4f9e94c2b65e08817b45da7df89431db59c9
Dan Reed <dan.reed@vistair.com>
2019-11-29 10:37:26 +0000

    Updates

---
+++ precache-manifest.680a5fc55eb6c520ca4776ac948c3b5d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893669f02dfdfd3b6ad213805603470e",
+    "url": "/heads-up-display/index.html"
+  },
+  {
+    "revision": "563d3ec0fb156811ce48",
+    "url": "/heads-up-display/static/css/2.9f564537.chunk.css"
+  },
+  {
+    "revision": "9b0cf8992c05b51377d7",
+    "url": "/heads-up-display/static/css/main.4ba4f923.chunk.css"
+  },
+  {
+    "revision": "563d3ec0fb156811ce48",
+    "url": "/heads-up-display/static/js/2.572e841b.chunk.js"
+  },
+  {
+    "revision": "9b0cf8992c05b51377d7",
+    "url": "/heads-up-display/static/js/main.0ef888f7.chunk.js"
+  },
+  {
+    "revision": "3f3ec0945639be23c571",
+    "url": "/heads-up-display/static/js/runtime-main.fa52908f.js"
+  },
+  {
+    "revision": "245691099757d6c16dc1535f3ac775a3",
+    "url": "/heads-up-display/static/media/CockpitWithSky.24569109.jpg"
+  },
+  {
+    "revision": "b4e92850c42059b81109e0d9173e7a07",
+    "url": "/heads-up-display/static/media/CockpitWithSkyAndFlightPath.b4e92850.jpg"
+  },
+  {
+    "revision": "9f2131c2974ff6dcfefe82f5727c466d",
+    "url": "/heads-up-display/static/media/background.9f2131c2.svg"
+  },
+  {
+    "revision": "7bd9a61f8f1ba09b4d7f957d243470d7",
+    "url": "/heads-up-display/static/media/compass.7bd9a61f.svg"
+  },
+  {
+    "revision": "adb137f0fa13eb025c13d13c727fe7fa",
+    "url": "/heads-up-display/static/media/grab.adb137f0.svg"
+  },
+  {
+    "revision": "3acfddd5c09c90a4f089f8d626aebca1",
+    "url": "/heads-up-display/static/media/grabbing.3acfddd5.svg"
+  },
+  {
+    "revision": "f37aa8ca72b5548a255c62dab1614437",
+    "url": "/heads-up-display/static/media/sprites.f37aa8ca.svg"
+  }
+]);
